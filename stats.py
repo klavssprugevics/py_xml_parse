38c58d807980c1d3a10dd42a44c284a31a8b2d05
unknown <klavs.sprugevics@gmail.com>
2021-01-21 10:30:09 +0200

    Basic statistics

--- stats.py
+++ stats.py
@@ -4,11 +4,13 @@ conn = sqlite3.connect("futbols")
 cursor = conn.cursor()
 
 
+# TODO: Row numbers everywhere
 # Turnira tabula
 
 for row in cursor.execute("SELECT nosaukums, punktu_sk, uzv_sk_pl, zaud_sk_pl, uzv_sk_pp, zaud_sk_pp, guto_vartu_sk, zaud_vartu_sk FROM Komanda ORDER BY punktu_sk DESC"):
     print(row)
 
+print("---------------------------------------------")
 # Turnira 10 rezultativakie speletaji
-
-# for row in cursor.execute("SELECT vards, uzvards, ")
+for row in cursor.execute("SELECT vards, uzvards, komanda, speletaja_nr, vartu_skaits, piespelu_skaits FROM Speletajs ORDER BY vartu_skaits DESC, piespelu_skaits DESC LIMIT 10"):
+    print(row)
